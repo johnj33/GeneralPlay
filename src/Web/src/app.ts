2f3eca1e83ca9f0d2bb08011de0542b52112efd5
john Jackson <johnj33@hotmail.co.uk>
2016-04-10 00:03:56 +0100

    play with font awesome and add random page change

--- src/Web/src/app.ts
+++ src/Web/src/app.ts
@@ -2,7 +2,10 @@ export class App {
     message: string = 'Page Number: ';
     pageNumber: number = 1;
     public changePage(value: number) {
-      this.pageNumber += value;
-      console.log("got here");
-}
+        this.pageNumber += value;
+        console.log("got here");
+    }
+    public ChangeToRandomPage(){
+       this.pageNumber = Math.floor((Math.random() * 10) + 1);
+    }
 }
