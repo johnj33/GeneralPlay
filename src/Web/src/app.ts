0cb6a53df0e5214723c0829d6f580d7a834d4ec5
john Jackson <johnj33@hotmail.co.uk>
2016-04-10 18:07:57 +0100

    seperate out page parts to own templates and pass states

--- src/Web/src/app.ts
+++ src/Web/src/app.ts
@@ -1,11 +1,5 @@
+import {bindable} from "aurelia-framework";
 export class App {
     message: string = 'Page Number: ';
-    pageNumber: number = 1;
-    public changePage(value: number) {
-        this.pageNumber += value;
-        console.log("got here");
-    }
-    public ChangeToRandomPage(){
-       this.pageNumber = Math.floor((Math.random() * 10) + 1);
-    }
+    public pageNumber: number = 1;
 }
